a8b7b167073d0bb1a38e64092e4248991dcfc2eb
Gerzon05 <gerzonarletrangel@gmail.com>
2024-07-22 18:11:40 -0500

    feat: Add Icons component for displaying icons

---
+++ components/Icons.jsx
@@ -0,0 +1,8 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+export function HomeIcon(props) {
+  return <FontAwesome name="home" size={24} color="white" {...props} />;
+}
+export function CircleInfoIcon(props) {
+  return <FontAwesome name="info-circle" size={24} color="white" {...props} />;
+}
