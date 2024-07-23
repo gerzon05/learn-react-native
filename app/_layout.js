99d614b13096ac350ad519230585d732dd6f9381
Gerzon05 <gerzonarletrangel@gmail.com>
2024-07-23 11:45:40 -0500

    feat: Update Layout component with Stack navigation and header customization

--- app/_layout.js
+++ app/_layout.js
@@ -1,10 +1,28 @@
-import { View } from 'react-native';
-import { Slot } from 'expo-router';
+import { Pressable, View } from 'react-native';
+import { Link, Stack } from 'expo-router';
+import { CircleInfoIcon } from '../components/Icons.jsx';
+import { Logo } from '../components/Logo.jsx';
 
 export default function Layout() {
   return (
     <View className="flex-1 bg-black">
-      <Slot />
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#000',
+          },
+          headerTintColor: '#fff',
+          headerTitle: '',
+          headerLeft: () => <Logo />,
+          headerRight: () => (
+            <Link href="/about" className="text-blue-500 w-10" asChild>
+              <Pressable className="bg-red-600 p-2 w-fit rounded-md">
+                <CircleInfoIcon />
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
     </View>
   );
 }
